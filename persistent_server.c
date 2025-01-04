f88775796660a9b3cf1cd503370e00c4d2f92195
Joy Herman <jherman50@stuy.edu>
2025-01-04 17:39:33 -0500

    added sig handler

--- persistent_server.c
+++ persistent_server.c
@@ -1,13 +1,18 @@
 #include "pipe_networking.h"
 
+void sighand(int sig) {
+	printf("server ended\n");
+	remove(WKP);
+	exit(0); 
+}
+
 int main() {
   int to_client;
   int from_client;
-	
+	signal(SIGINT, sighand);
 	while (1) {
 		from_client = server_handshake( &to_client );
 		close(from_client);
 		close(to_client);
 	}
-  
 }
